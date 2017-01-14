1869cc9b23b1c5f5e87ad1ce1aff83c5290b1f65
mark <markus.pluess@students.fhnw.ch>
2017-01-14 19:07:43 +0100

    Merging memberscsv

--- src/public/js/memberscsv.js
+++ src/public/js/memberscsv.js
@@ -4,7 +4,7 @@ let csvImportData = [];
 if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
 } else {
-  alert('The File APIs are not fully supported in this browser.');
+    alert('The File APIs are not fully supported in this browser.');
 }
 
 function handleFileSelect(evt) {
