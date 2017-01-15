3549a9a6567e81ba216d1520c72b1355eb9e293e
mark <markus.pluess@students.fhnw.ch>
2017-01-15 18:09:53 +0100

    Working on pie chart (page event)

--- src/public/js/charts.js
+++ src/public/js/charts.js
@@ -1,64 +1,22 @@
-// Load the Visualization API and the corechart package.
+// Loads the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
 function showPieChart(id, title, jData, width, height) {
-  // Create the data table.
+  // Creates the data table.
   var data = new google.visualization.DataTable(jData);
-  // Set chart options
+  // Sets chart options
   var options = {
     'title' : title,
     'width' : width,
     'height' : height};
-  // Instantiate and draw our chart, passing in some options.
+  // Instantiate and draw chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById(id));
   chart.draw(data, options);
   return chart;
 }
 
-function showColChart(id, title, jData, width, height) {
-  // Create the data table.
-  var data = new google.visualization.DataTable(jData);
-  // Set chart options
-  var options = {
-    'title' : title,
-    'width' : width,
-    'height' : height};
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.ColumnChart(document.getElementById(id));
-  chart.draw(data, options);
-}
-
-function showLineChart(id, title, jData, width, height) {
-  // Create the data table.
-  var data = new google.visualization.DataTable(jData);
-  // Set chart options
-  var options = {
-    'title' : title,
-    'width' : width,
-    'height' : height};
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.LineChart(document.getElementById(id));
-  chart.draw(data, options);
-}
-
-function showStackColChart(id, title, jData, width, height) {
-  // Create the data table.
-  var data = new google.visualization.DataTable(jData);
-  // Set chart options
-  var options = {
-    'title'     : title,
-    'width'     : width,
-    'height'    : height,
-    'isStacked' : true
-  };
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.ColumnChart(document.getElementById(id));
-  chart.draw(data, options);
-}
-
-
 function showStackedAreaChart(id, title, jData, width, height) {
-    // Create the data table.
+    // Creates the data table.
     var data = new google.visualization.DataTable(jData);
     // Set chart options.
     var options = {
@@ -67,7 +25,7 @@ function showStackedAreaChart(id, title, jData, width, height) {
         'height'    : height,
         'isStacked' : true
     };
-    // Instantiate and draw chart, passing in some options.
+    // Instantiates and draw chart, passing in some options.
     var chart = new google.visualization.AreaChart(document.getElementById(id));
     chart.draw(data, options);
 }
--- src/public/js/event.js
+++ src/public/js/event.js
@@ -6,5 +6,24 @@ window.app.page("event", () => {
     api(`organisation/${User.getData().organisation.id}/events/${eventid}`).then(event => {
       log(event);
     });
+    
+    setChartEventsReasons()
   }
 });
+
+function setChartEventsReasons() {
+    var jData = {};
+    jData.cols = [];
+    jData.cols[jData.cols.length] = {'id':'','label':'Topping','pattern':'','type':'string'};
+    jData.cols[jData.cols.length] = {'id':'','label':'Slices','pattern':'','type':'number'};
+    jData.rows = [];
+    jData.rows[jData.rows.length] = {'c':[{'v':'Anwesend','f':null},{'v':3,'f':null}]};
+    
+    api(`organisation/${User.getData().organisation.id}/reasons`).then(reasons => {
+        const reasonsData = Object.values(reasons);
+        for (i = 0; i < reasonsData.length; ++i) {
+            jData.rows[jData.rows.length] = {'c':[{'v': reasonsData[i].text,'f':null},{'v':3,'f':null}]};
+        }
+        showPieChart('chart-event-reasons', null, jData, 800, 500);
+    });
+}
